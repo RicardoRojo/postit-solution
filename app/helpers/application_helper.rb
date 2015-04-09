07cc5dff0dd73b63e94427f355884c8c29417030
Ricardo Rojo <ricardorojo@netscape.net>
2015-04-09 04:26:13 -0400

    added fix_url y format_date_time helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def fix_url(url)
+    url.starts_with?("http://") ? url : "http://#{url}"
+  end
+  def format_date_time(dt)
+    dt.strftime("%d/%m/%Y - %H:%M")
+  end
 end
