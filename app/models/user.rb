6d77d96f86537f798b20b7ed4e534ffc2821163f
Ricardo Rojo <ricardorojo@netscape.net>
2015-04-13 13:04:53 -0400

    added user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   has_many :comments
 
   has_secure_password validations: false
+
+  validates :username, uniqueness: true, presence: true, length: {minimum: 3, maximum: 20}
+  validates :password, presence: true, length: {minimum: 5, maximum: 20}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ PostitTemplate::Application.routes.draw do
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy"
+  get '/register', to: "users#new", as: "register"
+
+  resources :users, only: [:create]
 
   resources :posts, except: [:destroy] do
     resources :comments, only: [:create]
