1736e6b3a496a4dc64a0bce0689602ebdeb3cd38
Ricardo Rojo <ricardorojo@netscape.net>
2015-04-19 14:29:38 -0400

    added slug to user and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,14 @@ class Category < ActiveRecord::Base
   has_many :posts, through: :post_categories
   validates :name, presence: true
   validates :name, length: {minimum: 3, maximum: 25}
+
+  after_validation :add_slug
+
+  def to_param
+    self.slug
+  end
+
+  def add_slug
+    self.slug = self.name.downcase.gsub(" ","-")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,16 @@ class User < ActiveRecord::Base
   has_many :votes
   has_secure_password validations: false
 
+  after_validation :add_slug
+
   validates :username, uniqueness: true, presence: true, length: {minimum: 3, maximum: 20}
   validates :password, presence: true, length: {minimum: 5, maximum: 20}, on: :create
+
+  def to_param
+    self.slug
+  end
+
+  def add_slug
+    self.slug = self.username
+  end
 end
