09e51f03352df171849d91fba0e1434b40299357
Ricardo Rojo <ricardorojo@netscape.net>
2015-04-21 04:37:49 -0400

    created voteable module and extracted methods into the module

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,27 +1,16 @@
 class Post < ActiveRecord::Base
-  # belongs_to :user
+  include Voteable
+
   belongs_to :creator, foreign_key: "user_id", class_name: "User"
   has_many :comments
   has_many :post_categories
   has_many :categories, through: :post_categories
-  has_many :votes, as: :voteable
 
   validates :title, presence: true
   validates :url, presence: true
 
   after_validation :add_slug!
 
-  def total_votes
-    up_votes - down_votes
-  end
-
-  def up_votes
-    self.votes.where("vote = ?", true).size
-  end
-
-  def down_votes
-    self.votes.where("vote = ?", false).size
-  end
 
   def to_param
     self.slug
